4500ec06fd54550e9ebb918c3f7a6c7564cebe7d
mkbaraka <mikel.martinezz@opendeusto.es>
2022-04-01 16:41:29 +0200

    Event class Update

--- my-app/src/main/java/com/mycompany/app/basicClass/Event.java
+++ my-app/src/main/java/com/mycompany/app/basicClass/Event.java
@@ -1,29 +1,25 @@
 package com.mycompany.app.basicClass;
 
-import java.sql.Date;
 
 public class Event {
     private String name;
 	private String date;
-	private float price;
-	private String company;
+	private String place;
 
 
 
-	public Event(String name, String date, float price,
-			String company) {
+	public Event(String name, String date, 
+			String place) {
 		super();
 		this.name = name;
 		this.date = date;
-		this.price = price;
-		this.company = company;
+		this.place = place;
 	}
 	public Event() {
 		super();
 		this.name = "";
 		this.date = "";
-		this.price = 0;
-		this.company = "";
+		this.place = "";
 	}
 	
 	
@@ -41,23 +37,16 @@ public class Event {
 		this.date = date;
 	}
 
-	public float getPrice() {
-		return price;
+	public String getPlace() {
+		return place;
 	}
-	public void setPrice(Float price) {
-		this.price = price;
-	}
-
-	public String getCompany() {
-		return company;
-	}
-	public void setCompany(String company) {
-		this.company = company;
+	public void setPlace(String place) {
+		this.place = place;
 	}
 
 	
 	@Override
 	public String toString() {
-		return "Name: " + name + ", Date: " + date;
+		return "Name: " + name + ", Place: " + place + ", Date: " + date;
 	}
 }
