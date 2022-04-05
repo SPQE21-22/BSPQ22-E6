098fe8386d6cded68f0e457f317ff0abb2a5be71
mkbaraka <mikel.martinezz@opendeusto.es>
2022-04-05 18:13:15 +0200

    Event and ticket updates

--- my-app/src/main/java/com/mycompany/server/data/domain/Event.java
+++ my-app/src/main/java/com/mycompany/server/data/domain/Event.java
@@ -5,23 +5,25 @@ public class Event {
     private String name;
 	private String date;
 	private String place;
+	private Organizer organizer;
 
 
 
 	public Event(String name, String date, 
-			String place) {
+			String place, Organizer organizer) {
 		super();
 		this.name = name;
 		this.date = date;
 		this.place = place;
+		this.organizer = organizer
 	}
 	public Event() {
 		super();
 		this.name = "";
 		this.date = "";
 		this.place = "";
+		this.organizer = null;
 	}
-	
 	
 	public String getName() {
 		return name;
@@ -43,10 +45,16 @@ public class Event {
 	public void setPlace(String place) {
 		this.place = place;
 	}
+	public String getOrganizer() {
+		return organizer;
+	}
+	public void setOrganizer(Organizer organizer) {
+		this.organizer = organizer;
+	}
 
 	
 	@Override
 	public String toString() {
-		return "Name: " + name + ", Place: " + place + ", Date: " + date;
+		return "Name: " + name + ", Place: " + place + ", Date: " + date + ", Organizer: " + organizer;
 	}
 }
--- my-app/src/main/java/com/mycompany/server/data/domain/Ticket.java
+++ my-app/src/main/java/com/mycompany/server/data/domain/Ticket.java
@@ -1,8 +1,31 @@
 package com.mycompany.server.data.domain;
 
 public class Ticket {
-	//TODO
+	private Event Event;
+	private Organizer organizer;
+	
 	public Ticket(Event e, Consumer c) {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public String getEvent() {
+		return event;
+	}
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+
+	public String getOrganizer() {
+		return organizer;
+	}
+	public void setOrganizer(Organizer organizer) {
+		this.organizer = organizer;
+	}
+	
+	@Override
+	public String toString() {
+		return "Event: " + event + ", Organizer: " + organizer;
+	}
+	
+	
 }
