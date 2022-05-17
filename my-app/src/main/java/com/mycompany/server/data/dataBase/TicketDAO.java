f2d1195ecad5a9e25a3f2d3f37bfa9e8c1fe9a02
Guillermo Perez <guillermo.perez@opendeusto.es>
2022-05-17 20:21:34 +0200

    DAOs finished with minor bugs in testing pair programmed with @DanelArias-Dreyton257 #BSPQ22E6-71 add work 1h Development

--- my-app/src/main/java/com/mycompany/server/data/dataBase/TicketDAO.java
+++ my-app/src/main/java/com/mycompany/server/data/dataBase/TicketDAO.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.jdo.Transaction;
 
+import com.mycompany.server.data.domain.Consumer;
+import com.mycompany.server.data.domain.Event;
 import com.mycompany.server.data.domain.Ticket;
 
-public class TicketDAO extends DataAccesObjectBase implements IDataAccesObject<Ticket>{
-	
+public class TicketDAO extends DataAccesObjectBase implements IDataAccesObject<Ticket> {
+
 	private static TicketDAO instance;
-	
+
 	@Override
 	public void save(Ticket c) {
 		super.saveObject(c);
@@ -23,32 +26,47 @@ public class TicketDAO extends DataAccesObjectBase implements IDataAccesObject<T
 	}
 
 	@Override
-	public Ticket find(String param) {
-		PersistenceManager pm = pmf.getPersistenceManager();
-		Transaction tx = pm.currentTransaction();
+	/**
+	 * Finds a consumer in the DB
+	 * 
+	 * @param String, three parameters: 1-Name of the event of the ticket 2-Date of
+	 *                the Event (Format: YYYY-MM-DD) 3- Email of the Consumer
+	 * @return the ticket object
+	 */
+	public Ticket find(String... params) {
+		if (params.length != 3) {
+			return null;
+		}
 
-		Ticket c = null;
-		/*
+		PersistenceManager pm3 = pmf.getPersistenceManager();
+		Transaction tx3 = pm3.currentTransaction();
+
+		Ticket t = null;
+
 		try {
-			
-			tx.begin();
 
-			Query<?> query = pm.newQuery("SELECT FROM " + User.class.getName() + " WHERE email == '" + param + "'");
+			tx3.begin();
+			Query<?> query = pm3.newQuery("SELECT FROM " + Ticket.class.getName()
+					+ " WHERE event_event_id_oid IN (SELECT event_id FROM " + Event.class.getName() + " WHERE name == '"
+					+ params[0] + "' AND" + " date == date('" + params[1] + "')) AND ticket_id IN (SELECT ticket_id_eid FROM consumer_boughttickets"
+							+ "WHERE consumer_id_oid IN (SELECT consumer_id FROM "+ Consumer.class.getName() + " WHERE email == '" +params[2] + "'))");
+		
+
 			query.setUnique(true);
-			u = (User) query.execute();
-
-			tx.commit();
+			t = (Ticket) query.execute();
+			tx3.commit();
 		} catch (Exception ex) {
-			System.out.println("  $ Error querying a User: " + ex.getMessage());
+			System.out.println("  $ Error querying a Ticket: " + ex.getMessage());
 		} finally {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
+			if (tx3 != null && tx3.isActive()) {
+				tx3.rollback();
 			}
 
-			pm.close();
+			pm3.close();
 		}
-	*/
-		return c;
+
+		return t;
+
 	}
 
 	public static TicketDAO getInstance() {
@@ -66,26 +84,18 @@ public class TicketDAO extends DataAccesObjectBase implements IDataAccesObject<T
 
 		List<Ticket> users = new ArrayList<>();
 		/*
-		try {
-			tx.begin();
-
-			Extent<User> extent = pm.getExtent(User.class, true);
-
-			for (User el : extent) {
-				users.add(el);
-			}
-
-			tx.commit();
-		} catch (Exception ex) {
-			System.out.println("  $ Error retrieving all the users: " + ex.getMessage());
-		} finally {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-
-			pm.close();
-		}
-		*/
+		 * try { tx.begin();
+		 * 
+		 * Extent<User> extent = pm.getExtent(User.class, true);
+		 * 
+		 * for (User el : extent) { users.add(el); }
+		 * 
+		 * tx.commit(); } catch (Exception ex) {
+		 * System.out.println("  $ Error retrieving all the users: " + ex.getMessage());
+		 * } finally { if (tx != null && tx.isActive()) { tx.rollback(); }
+		 * 
+		 * pm.close(); }
+		 */
 		return users;
 	}
 }
